3fa0097a50e18120ee2f23039df3dcd98250059b
mannepalli uday kumar <65896248+uk8380@users.noreply.github.com>
2021-06-18 11:53:12 +0530

    Add files via upload

---
+++ for loop.py
@@ -0,0 +1,32 @@
+uk=["uday",99,00,99,123]
+
+for x in uk:
+    print(x)
+
+#loop through str
+for x in "uday":
+    print(x)
+
+#break
+love=["uday",99,"bam"]
+for x in love:
+    print(x)
+    if(x=="bam"):
+        break
+        print(x)
+
+#continue
+for i in love:
+    if(x=="uday"):
+        continue
+    print(i)
+#range
+for a in range(2,7):
+    print(a)
+    if(a==6):
+        print("completed!")
+
+#pass
+for u in love:
+    pass #null value
+        
